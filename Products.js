7058a1c5f94c019e2eab1576e41233375bcf9fda
Akshar Chanchlani <aksharchanchlani7006@gmail.com>
2024-10-18 22:27:36 +0100

    Add checkout functionality and view switching

---
+++ Products.js
@@ -0,0 +1,101 @@
+let products = [
+    {
+      "id": 1,
+      "name": "Maths",
+      "location": "London",
+      "price": "£ 100",
+      "availableSpaces": 10,
+      "image": "./Images/PMaths-pxckife9xotc2esxcod13tks9h2zru0w73ah75dby0.jpeg",
+      "addedToCart": false,
+      "rating": 3
+    },
+    {
+      "id": 1,
+      "name": "Maths",
+      "location": "Manchester",
+      "price": "£ 100",
+      "availableSpaces": 8,
+      "image": "./Images/PMaths-pxckife9xotc2esxcod13tks9h2zru0w73ah75dby0.jpeg",
+      "addedToCart": false,
+      "rating": 4
+    },
+    {
+      "id": 2,
+      "name": "Chemistry",
+      "location": "London",
+      "price": "£ 130",
+      "availableSpaces": 4,
+      "image": "./Images/1000_F_438697732_VOA5exdnvidqFuC2M3koVPNOqikvc8sy.jpg",
+      "addedToCart": false,
+      "rating": 5
+    },
+    {
+      "id": 2,
+      "name": "Chemistry",
+      "location": "Edinburgh",
+      "price": "£ 140",
+      "availableSpaces": 11,
+      "addedToCart": false,
+      "rating": 4,
+      "image": "./Images/1000_F_438697732_VOA5exdnvidqFuC2M3koVPNOqikvc8sy.jpg"
+    },
+    {
+      "id": 3,
+      "name": "English",
+      "location": "Cambridge",
+      "price": "£ 150",
+      "availableSpaces": 16,
+      "addedToCart": false,
+      "rating": 5,
+      "image": "./Images/collection-colored-thin-icon-of-english-language-learning-subject-book-graduated-hat-learning-and-education-concept-illustration-vector.jpg"
+    },
+    {
+      "id": 3,
+      "name": "English",
+      "location": "Oxford",
+      "price": "£ 160",
+      "availableSpaces": 0,
+      "addedToCart": false,
+      "rating": 5,
+      "image": "./Images/collection-colored-thin-icon-of-english-language-learning-subject-book-graduated-hat-learning-and-education-concept-illustration-vector.jpg"
+    },
+    {
+      "id": 4,
+      "name": "Physics",
+      "location": "London",
+      "price": "£ 170",
+      "availableSpaces": 13,
+      "addedToCart": false,
+      "rating": 4,
+      "image": "./Images/physics-subject-conceptlettering-card-vector-600w-1110756503.webp"
+    },
+    {
+      "id": 4,
+      "name": "Physics",
+      "location": "Scotland",
+      "price": "£ 170",
+      "availableSpaces": 11,
+      "addedToCart": false,
+      "rating": 4,
+      "image": "./Images/physics-subject-conceptlettering-card-vector-600w-1110756503.webp"
+    },
+    {
+      "id": 5,
+      "name": "Music",
+      "location": "Wales",
+      "price": "£ 170",
+      "availableSpaces": 14,
+      "addedToCart": false,
+      "rating": 1,
+      "image": "./Images/ttttttttttttttttttttt-01-773.webp"
+    },
+    {
+      "id": 5,
+      "name": "Physics",
+      "location": "Northern Ireland",
+      "price": "£ 170",
+      "availableSpaces": 2,
+      "rating": 3,
+      "image": "./Images/ttttttttttttttttttttt-01-773.webp"
+    },
+  ]
